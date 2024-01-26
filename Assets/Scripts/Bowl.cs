63b8f51c891d5ee9a8ec1f37cee4a2b488537226
Alyssa <alyssacanlas20@gmail.com>
2024-01-26 12:07:22 +0200

    Added bowl and mixing mechanic WIP

---
+++ Assets/Scripts/Bowl.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Bowl : MonoBehaviour
+{
+    private bool isLocked = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the object is locked
+        if (isLocked)
+        {
+            // Object is locked, it won't perform its usual actions
+            Debug.Log("Object is locked. Waiting for toggle...");
+        }
+        else
+        {
+            // Object is unlocked, perform its usual actions
+            PerformObjectActions();
+        }
+
+        // Check for toggle event (you can replace this with your specific event condition)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ToggleLock();
+        }
+    }
+
+    void PerformObjectActions()
+    {
+        // Implement the usual actions the object should perform when unlocked
+        Debug.Log("Performing object actions...");
+    }
+
+    void ToggleLock()
+    {
+        // Toggle the lock state
+        isLocked = !isLocked;
+
+        // Print a message to indicate the lock state change
+        if (isLocked)
+        {
+            Debug.Log("Object is now locked.");
+        }
+        else
+        {
+            Debug.Log("Object is now unlocked.");
+        }
+    }
+}
--- Assets/Scripts/MixMechanic.cs
+++ Assets/Scripts/MixMechanic.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class MixMechanic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // this script checks if something interactable is inside the bowl and if so make it disappear and go
+    public Transform tpDestination;
+    public float waitTime;
+
+    private bool isTp;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Interactable") && !isTp)
+        {
+            Debug.Log("Interactable inside");
+            
+            StartCoroutine(TpAfterDelay(other.transform)); //tp item
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator TpAfterDelay(Transform objectToTeleport)
     {
-        
+        isTp = true;
+
+        yield return new WaitForSeconds(waitTime);
+
+        objectToTeleport.position = tpDestination.position;
+
+        isTp = false;
     }
 }
